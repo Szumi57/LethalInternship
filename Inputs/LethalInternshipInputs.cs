fa21d8857153d95c0791aa21aa20817a67abc36f
Szumi <szumigala.david@outlook.fr>
2025-02-01 19:28:08 +0100

    input class publified

--- Inputs/LethalInternshipInputs.cs
+++ Inputs/LethalInternshipInputs.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 
 namespace LethalInternship.Inputs
 {
-    internal class LethalInternshipInputs : LcInputActions
+    public class LethalInternshipInputs : LcInputActions
     {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
