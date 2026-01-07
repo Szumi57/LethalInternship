e6ec516bc8c14ad47e7a2f59fa790ec6f21fd46b
Szumi <szumigala.david@outlook.fr>
2026-01-07 18:08:07 +0100

    0.24.0 debug true

--- LethalInternship.SharedAbstractions/Constants/DebugConst.cs
+++ LethalInternship.SharedAbstractions/Constants/DebugConst.cs
@@ -3,10 +3,10 @@
     public class DebugConst
     {
         // Debug
-        public static readonly bool DRAW_LINES = false;
+        public static readonly bool DRAW_LINES = true;
         public static readonly bool SHOW_LOG_DEBUG_ONTRANSFORMPARENTCHANGED = false;
-        public static readonly bool SPAWN_INTERN_WITH_EMOTE = false;
-        public static readonly bool NO_DAMAGE = false;
+        public static readonly bool SPAWN_INTERN_WITH_EMOTE = true;
+        public static readonly bool NO_DAMAGE = true;
         public static readonly bool NO_DEATH = false;
         public static readonly bool SPAWN_BUSH_WOLVES_FOR_DEBUG = false;
         public static readonly bool SPAWN_MINESHAFT_FOR_DEBUG = false;
